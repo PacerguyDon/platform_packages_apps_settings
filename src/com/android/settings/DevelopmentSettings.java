e51dbc0e7e23794e598e1c1250a1510bed6d9814
blunden <blunden2@gmail.com>
2012-08-14 00:44:27 +0200

    Settings: Use holo alert drawable
    Includes fixes from CM9 and new ones for AOSP issues
    
    Change-Id: I31a2fbf05244bccfc893a2f3a7f91087ba94d3cc

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -774,7 +774,7 @@ public class DevelopmentSettings extends PreferenceFragment
                             getActivity().getResources().getString(
                                     R.string.dev_settings_warning_message))
                             .setTitle(R.string.dev_settings_warning_title)
-                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setIconAttribute(android.R.attr.alertDialogIcon)
                             .setPositiveButton(android.R.string.yes, this)
                             .setNegativeButton(android.R.string.no, this)
                             .show();
@@ -817,7 +817,7 @@ public class DevelopmentSettings extends PreferenceFragment
                 mAdbDialog = new AlertDialog.Builder(getActivity()).setMessage(
                         getActivity().getResources().getString(R.string.adb_warning_message))
                         .setTitle(R.string.adb_warning_title)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setIconAttribute(android.R.attr.alertDialogIcon)
                         .setPositiveButton(android.R.string.yes, this)
                         .setNegativeButton(android.R.string.no, this)
                         .show();
@@ -826,6 +826,7 @@ public class DevelopmentSettings extends PreferenceFragment
                 Settings.Secure.putInt(getActivity().getContentResolver(),
                         Settings.Secure.ADB_ENABLED, 0);
             }
+
         } else if (preference == mKeepScreenOn) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STAY_ON_WHILE_PLUGGED_IN, 
